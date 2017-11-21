0a5b9ed535682841f28fe4410cf1b2cf893faabc
Praful <oprafulramesh@gmail.com>
2017-11-21 23:18:59 +0530

    Updated hex code

--- hex_chk.cpp
+++ hex_chk.cpp
@@ -4,17 +4,28 @@ using namespace std;
 int main()
 {
 	float output;
-	int i;
+	int j,i;
 	float x; 
 	float o1, o2, o3, o4;
 	int y1, y2, y3, y4;
+	
+	i = 0x790;
 
-	i = 0xc95;
+	if (i > 0x7F0)
+	{
+		i = 0xfffff000 | i;
+		cout << std::hex << i << endl;
+		i = ~i;
+		i = i+1;
+		j = 1;
+	}
+	else
+	{
+		i = i;	
+	}
 	
 	x = i * 0.0625;
 	
-	cout << "Value of x = " << x << endl;
-
 	y1 = i & 0x0001;
 	y2 = i & 0x0002;
 	y3 = i & 0x0004;
@@ -28,9 +39,13 @@ int main()
 	o2 = y2 * 0.125;
 	o3 = y3 * 0.25;
 	o4 = y4 * 0.5;
-	cout << "o1: " << o1 <<"\n"<<"o2: " << o2 << "\n" <<"o3: " <<  o3 << "\n" <<"o4: "<<  o4 << endl;
 
 	output = x + o1 + o2 + o3 + o4;
 	
+	if(j)
+		output = output * (-1);
+	else
+		output = output;
+
 	cout << "Floating point value: "<< output << endl;
 }
