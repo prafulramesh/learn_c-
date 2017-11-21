cc8451b99fcbf2667bccf1077d3151b0cc0d73d3
VKCHCP0030 <oprafulramesh@gmail.com>
2017-11-21 19:33:58 +0530

    Update

--- hex_chk.cpp
+++ hex_chk.cpp
@@ -9,9 +9,12 @@ int main()
 	float o1, o2, o3, o4;
 	int y1, y2, y3, y4;
 
-	i = 0xc90;
+	i = 0xc95;
 	
 	x = i * 0.0625;
+	
+	cout << "Value of x = " << x << endl;
+
 	y1 = i & 0x0001;
 	y2 = i & 0x0002;
 	y3 = i & 0x0004;
@@ -25,8 +28,9 @@ int main()
 	o2 = y2 * 0.125;
 	o3 = y3 * 0.25;
 	o4 = y4 * 0.5;
+	cout << "o1: " << o1 <<"\n"<<"o2: " << o2 << "\n" <<"o3: " <<  o3 << "\n" <<"o4: "<<  o4 << endl;
 
 	output = x + o1 + o2 + o3 + o4;
 	
-	cout << "Floating point value"<< output << endl;
+	cout << "Floating point value: "<< output << endl;
 }
