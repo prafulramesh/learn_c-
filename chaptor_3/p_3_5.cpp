d7df19e41ac1057b7de036052295d9c32a405602
Praful <oprafulramesh@gmail.com>
2017-11-25 23:27:40 +0530

    No changes

---
+++ chaptor_3/p_3_5.cpp
@@ -0,0 +1,19 @@
+// Explicit type casting
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int intvar=25;
+	float floatvar=35.87;
+
+	cout <<"intvar = " << intvar << endl;
+	cout <<"floatvar = " << floatvar << endl;
+	cout <<"float(intvar) = " << float(intvar) << endl;
+	cout <<"int(floatvar) = " << int(floatvar) << endl;
+	
+}
+
+
